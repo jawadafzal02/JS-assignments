8ea0bbabc5adcd24526d2ce3297c2bbc2ab02cc8
jawadafzal02 <123592492+jawadafzal02@users.noreply.github.com>
2023-03-19 18:19:17 +0500

    Add files via upload

---
+++ Javascript/Assigment 6/app.js
@@ -0,0 +1,78 @@
+// Q1
+// let firstName = prompt("Enter your first name");
+// let lastName = prompt("Enter your last name");
+// let fullName = firstName + " " + lastName;
+
+// alert ("hello " + fullName + "!");
+
+// Q2
+// let mobileName = prompt("Your favourite mobile name");
+// let length = favouritePhoneModel.lenght;
+// document.write("your favourite mobile name" + lenght + "character")
+
+// Q3
+// let str = "pakistani";
+// document.write("string pakistani" );
+// document,write(<\br>)
+// document.write( str.indexOf("n"));
+
+// Q4
+// let greet = "hello world";
+// console.log(greet.lastIndexOf("l"))
+
+// document.write(string + hello + world);
+
+// Q5
+// let word = "pakistani";
+// var character = word.charAt(3);
+// document.write('the character on index 3 is' ":" + character  );
+
+// Q6
+// var city = "hydrabad"
+// document.write("city :  hydrabad")
+// var newCity = city.replace('hydrabad' , 'islmabad')
+//  document.write(newCity)
+
+// var intro = "my name is huzaifa"
+// document.write(intro)
+// document.write(intro.replace("huzaifa", "jawad"));
+
+// var message = "ali and sami are good friends they play cricket and football together"
+// var updateMessage = message.replace(/and/g , "&")
+// document.write(updateMessage)
+
+
+// var num = "472"
+// var  newNum = Number(num);
+// console.log(num);
+
+// let input = prompt("enter your message");
+
+// let capitalize = input.toUpperCase();
+
+// console.log(input: ${input});
+// console.log( capitalize : ${capitalize} )
+
+// let input = prompt("Enter a string:");
+
+// Convert string to all capital letters
+// let capitalized = input.toUpperCase();
+
+// console.log(`Input: ${input}`);
+// console.log(` ${capitalized}`);
+
+
+// var str = "javascript"
+// var newStr = str.toTitleCase();
+// console.log(newStr);
+
+// let str = 35.46
+// let newStr = str.toString().replace("." ,"")
+// document.write(newStr)
+// console.log(typeof newStr);
+
+
+
+let num = 34.65
+let str = num.toString().replace("." ,"")
+document.write(str)
